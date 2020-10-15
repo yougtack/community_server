37e181bb4f28c3b5bcbae2d41534b94a963c3faa
김영택 <yt.kim@how2marry.com>
2020-10-15 14:14:48 +0900

    ...

--- src/main/java/com/community/model/BoardModel.java
+++ src/main/java/com/community/model/BoardModel.java
@@ -3,20 +3,20 @@ package com.community.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
 public class BoardModel {
     private Integer b_id;
+    private Integer b_recomment_id;
     private String b_type;
     private String b_title;
     private Long b_date;
     private int b_count;
     private String userId;
 
-    private List<BoardComment> boardComments;
+//    private List<BoardComment> boardComments;
 //    private List<CommentModel> comments;
     private int commentCount;
 }
