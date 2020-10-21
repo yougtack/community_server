1a688902d89d023889b40a07cddcb378ef8df316
김영택 <yt.kim@how2marry.com>
2020-10-21 16:53:15 +0900

    ...

--- src/main/java/com/community/model/BoardComment.java
+++ src/main/java/com/community/model/BoardComment.java
@@ -1,7 +1,10 @@
 package com.community.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -11,7 +14,8 @@ public class BoardComment {
     private String b_type;
     private String b_title;
     private String b_content;
-    private Long b_date;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Date b_date;
     private int b_count;
     private String userId;
 
--- src/main/java/com/community/model/ViewCommentModel.java
+++ src/main/java/com/community/model/ViewCommentModel.java
@@ -1,13 +1,18 @@
 package com.community.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
 public class ViewCommentModel {
     private Integer c_id;
     private String c_content;
-    private String c_date;
+
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Date c_date;
     private String userId;
 }
