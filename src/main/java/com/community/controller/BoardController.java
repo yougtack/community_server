e77650d36611f3400f22f832521753da611ea407
김영택 <yt.kim@how2marry.com>
2020-10-29 15:27:03 +0900

    사진업로드 바이너리코드 형태말고 URL로 들어 가게함

--- src/main/java/com/community/controller/BoardController.java
+++ src/main/java/com/community/controller/BoardController.java
@@ -14,12 +14,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
