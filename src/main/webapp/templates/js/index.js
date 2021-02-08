60dfaf1349959f12f019d53be33a2b62facb0416
youngtack <rladudxor59@naver.com>
2021-02-08 16:08:16 +0900

    주소 변경

--- src/main/webapp/templates/js/index.js
+++ src/main/webapp/templates/js/index.js
@@ -9,7 +9,7 @@ let c_length = 0;
     let xhttp = new XMLHttpRequest();
     const url = "http://3.133.28.138:8080";
 
-    xhttp.open("GET", URL + `/board/rank`, false);
+    xhttp.open("GET", url + `/board/rank`, false);
 
     xhttp.onreadystatechange = () => {
         if (xhttp.status !== 200) {
